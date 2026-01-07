8b14a384ffec3ec050a1e711cc67dd6e13874be2
paysafecarddkk-stack <paysafecarddkk@gmail.com>
2026-01-07 22:50:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("voucherForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Coupon ajouté avec succès.");
+});
+
+function activateCoupon() {
+  alert("Activation en cours...\nVeuillez patienter.");
+}
